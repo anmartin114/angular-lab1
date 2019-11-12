aa7f35b101af1c9c0ea02f5184d20449acce6da1
Alicia <anmartin114@gmail.com>
2019-11-12 11:59:17 -0500

    part2

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,16 +7,45 @@ import { Todo } from "../interfaces/todo";
   styleUrls: ["./todo.component.css"]
 })
 export class TodoComponent implements OnInit {
-  post: Todo;
+  taskList: Todo[] = [
+    { task: "fold clothes", completed: false },
+    { task: "feed dog", completed: false },
+    { task: "dishes", completed: false },
+    { task: "contemplate the curse of existence", completed: false },
+    { task: "grocery shopping", completed: false }
+  ];
 
-  title = "Todo List";
-  filter = "Filter your to-dos";
-  tasks = ["laundry", "dishes", "feed dog", "grocery shop"];
-  completeButton = "Complete";
-  xButton = "x";
-  addButton = "Add";
+  newTask: Todo = {
+    task: "",
+    completed: false
+  };
 
-  constructor() {}
+  newTodoText: string = "";
+  filterText: string = "";
+
+  addTask() {
+    this.taskList = [
+      ...this.taskList,
+      { task: this.newTodoText, completed: false }
+    ];
+    this.newTodoText = "";
+  }
+
+  done(todo: Todo): void {
+    todo.completed = true;
+  }
+
+  delete(todo: Todo): void {
+    this.taskList = this.taskList.filter(x => x !== todo);
+  }
+
+  getFilteredTodos() {
+    if (!this.filterText) {
+      return this.taskList;
+    }
+    const match = this.filterText;
+    return this.taskList.filter(todo => todo.task.includes(match));
+  }
 
   ngOnInit() {}
 }
